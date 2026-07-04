6787564fad61142e1e03c7fdffe77df225137ad7
Stevy Endaman <stevyabessolo@gmail.com>
2026-07-04 00:23:58 -0400

    chore: normalize line endings via gitattributes and update rector config

--- rector.php
+++ rector.php
@@ -6,18 +6,17 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/config',
-        __DIR__ . '/src',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
+        __DIR__.'/config',
+        __DIR__.'/src',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ])
     ->withSets([
 
     ])
+    ->withTypeCoverageLevel(8)
+    ->withCodeQualityLevel(0)
     ->withPreparedSets(
         deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
         earlyReturn: true,
-        strictBooleans: true,
     );
